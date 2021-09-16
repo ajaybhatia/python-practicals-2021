d432c8996c9a45490c7e09999e846d7aae98bde4
ajaybhatia <prof.ajaybhatia@gmail.com>
2021-09-16 08:46:59 +0530

    Practical 1

---
+++ assign1.py
@@ -0,0 +1,15 @@
+'''
+Practical 1
+
+Compute sum, subtraction, multiplication, division and exponent of given variables
+input by the user.
+'''
+
+a = int(input("Enter value of first number: "))
+b = int(input("Enter value of second number: "))
+
+print(f"Sum of {a} and {b} is {a+b}")
+print(f"Subtraction of {a} and {b} is {a-b}")
+print(f"Product of {a} and {b} is {a*b}")
+print(f"Division of {a} and {b} is {a/b}")
+print(f"Exponent of {a} and {b} is {a**b}")
